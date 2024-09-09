1caca5b4835b86a4dccad1b9668f9c838bb9addf
thdtjdgus <songsh9570@naver.com>
2024-09-09 13:41:52 +0900

    Update market_info_North.js

--- market_info_North.js
+++ market_info_North.js
@@ -1,5 +1,17 @@
 // 충주, 단양, 제천
 
 var polygonPath_North = {
-    
+     "제천약초시장 다각형" : [ 
+new kakao.maps.LatLng(37.1267167067214, 128.2175418692972 ), 
+new kakao.maps.LatLng(37.12636598537448, 128.218522892583 ), 
+new kakao.maps.LatLng(37.12638202089252, 128.2187739794309 ), 
+new kakao.maps.LatLng(37.12751048997715, 128.2199124863954 ), 
+new kakao.maps.LatLng(37.12739815224481, 128.2203749290683 ), 
+new kakao.maps.LatLng(37.12911335078374, 128.2208781009425 ), 
+new kakao.maps.LatLng(37.12927532560607, 128.2205694583373 ), 
+new kakao.maps.LatLng(37.12899864237951, 128.2204474925527 ), 
+new kakao.maps.LatLng(37.12912109528475, 128.2201676244183 ), 
+new kakao.maps.LatLng(37.12963301688721, 128.2203194586069 ), 
+new kakao.maps.LatLng(37.12988866603516, 128.219278045395 ), 
+],
 }
