c1efb6948facc65e1652aa9ce8318e33d151bf50
cbsmba <80944368+cbsmba@users.noreply.github.com>
2024-09-24 10:22:27 +0900

    Update market_info_South.js

--- market_info_South.js
+++ market_info_South.js
@@ -2,6 +2,16 @@
 
 var polygonPath_South = {
 
+  "영동전통시장" : [ 
+      new kakao.maps.LatLng(36.17845541891501, 127.7750884896691 ), 
+      new kakao.maps.LatLng(36.17777260593731, 127.7736294121847 ), 
+      new kakao.maps.LatLng(36.17658453393349, 127.7737971636314 ), 
+      new kakao.maps.LatLng(36.17580624993743, 127.7742684394236 ), 
+      new kakao.maps.LatLng(36.1740116737253, 127.7752048587474 ), 
+      new kakao.maps.LatLng(36.17537092097049, 127.7775726975752 ), 
+  ],
+  
+
   "영동중앙시장" : [ 
 new kakao.maps.LatLng(36.17478956302005, 127.7754885026421 ), 
 new kakao.maps.LatLng(36.17466642427551, 127.7755903186715 ), 
